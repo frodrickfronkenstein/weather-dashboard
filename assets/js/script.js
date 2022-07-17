a2f537307b0b6ae686a814f57d3e940e31590312
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-07-17 15:41:03 -0400

    current weather data found

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 const APIKey = "1c0b296429a846ab169e7fc204e8759b"
 
 function getWeather() {
-    var cityName = "Burlington";
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey
+    var lat = "42.32";
+    var lon = "-79.79";
+    var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + APIKey + "&units=metric"
 
     fetch(requestUrl)
     .then(function(response) {
@@ -10,6 +11,14 @@ function getWeather() {
     })
     .then(function(data) {
         console.log(data)
+        //var currentTemp = 
+        console.log(data.current.feels_like)
+        //var currentWind = 
+        console.log(data.current.wind_speed)
+        //var currentHum = 
+        console.log(data.current.humidity)
+        //var currentUV = 
+        console.log(data.current.uvi)
     });
 }
 
